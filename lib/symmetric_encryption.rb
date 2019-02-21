c05c896eefbd2ca4618ec6af9f23fc449095ebef
Reid Morrison <reidmo@gmail.com>
2019-02-21 15:42:19 -0500

    Use ActiveSupport.on_load when available

--- lib/symmetric_encryption.rb
+++ lib/symmetric_encryption.rb
@@ -7,14 +7,18 @@ begin
 rescue LoadError
 end
 
-ActiveSupport.on_load(:active_record) do
-  require 'symmetric_encryption/railties/attr_encrypted'
-  require 'symmetric_encryption/railties/symmetric_encryption_validator'
+begin
+  require 'active_support'
+  ActiveSupport.on_load(:active_record) do
+    require 'symmetric_encryption/railties/attr_encrypted'
+    require 'symmetric_encryption/railties/symmetric_encryption_validator'
 
-  ActiveRecord::Base.include(SymmetricEncryption::Railties::AttrEncrypted)
-end
+    ActiveRecord::Base.include(SymmetricEncryption::Railties::AttrEncrypted)
+  end
 
-ActiveSupport.on_load(:mongoid) do
-  require 'symmetric_encryption/railties/mongoid_encrypted'
-  require 'symmetric_encryption/railties/symmetric_encryption_validator'
+  ActiveSupport.on_load(:mongoid) do
+    require 'symmetric_encryption/railties/mongoid_encrypted'
+    require 'symmetric_encryption/railties/symmetric_encryption_validator'
+  end
+rescue LoadError
 end
