a0ab2ec6a1d86907db21cc4d7e7f433d923f92f2
Jared Hoyt <jaredhoyt@gmail.com>
2019-04-17 12:55:03 -0700

    Make EncryptedAttribute readers public

--- lib/symmetric_encryption/active_record/encrypted_attribute.rb
+++ lib/symmetric_encryption/active_record/encrypted_attribute.rb
@@ -1,6 +1,8 @@
 module SymmetricEncryption
   module ActiveRecord
     class EncryptedAttribute < ::ActiveRecord::Type::String
+      attr_reader :random_iv, :compress, :type
+
       def initialize(random_iv: true, compress: false, type: :string)
         @random_iv = random_iv
         @compress  = compress
@@ -14,10 +16,6 @@ module SymmetricEncryption
       def serialize(value)
         SymmetricEncryption.encrypt(value, type: type, compress: compress, random_iv: random_iv) if value
       end
-
-      private
-
-      attr_reader :random_iv, :compress, :type
     end
   end
 end
