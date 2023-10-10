fcf0c4a86680ef3523098e3ebb9df959ebb4925e
Sudheer <sudheer.meka@andela.com>
2023-10-10 16:22:58 +0530

    Dependant conditions added

--- lib/symmetric_encryption.rb
+++ lib/symmetric_encryption.rb
@@ -13,13 +13,16 @@ begin
     require "symmetric_encryption/active_record/attr_encrypted"
     require "symmetric_encryption/railties/symmetric_encryption_validator"
 
-    ActiveRecord::Type.register(:encrypted, SymmetricEncryption::ActiveRecord::EncryptedAttribute)
-    ActiveRecord::Type.register(:encrypted_json, SymmetricEncryption::ActiveRecord::EncryptedJsonAttribute)
-    ActiveRecord::Base.include(SymmetricEncryption::ActiveRecord::CustomEncryption)
-  
+    if ActiveRecord.version >= Gem::Version.new("5.0.0")
+      ActiveRecord::Type.register(:encrypted, SymmetricEncryption::ActiveRecord::EncryptedAttribute)    
+    end
+    
     # Remove old way of defining attributes with Rails 7 since it conflicts with the method names.
     if ActiveRecord.version <= Gem::Version.new("7.0.0")
       ActiveRecord::Base.include(SymmetricEncryption::ActiveRecord::AttrEncrypted)
+    else
+      ActiveRecord::Type.register(:encrypted_json, SymmetricEncryption::ActiveRecord::EncryptedJsonAttribute)
+      ActiveRecord::Base.include(SymmetricEncryption::ActiveRecord::CustomEncryption)  
     end
   end
 
